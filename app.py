3626aab5e661ced07ab4212af4ca5fa450dabf20
keyzen <alonso.anavalon@gmail.com>
2021-06-19 18:08:50 -0400

    haciendo tarjetas de cotizacion

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 #Modulos necesarios
 
+from pymysql import get_client_info
 from database import database
 from flask import Flask, redirect, url_for, request, jsonify, render_template
 
@@ -8,50 +9,189 @@ from flask import Flask, redirect, url_for, request, jsonify, render_template
 app = Flask(__name__)
 db = database.DataBase()
 
-#Rutas
+                                                #Rutas
 
+#Ruta de inicio
 @app.route('/', methods=["GET", "POST"])
 def index():
     if request.method == "GET":
-        print("Entraron al index")
-        return render_template("index.html")
+        fabricators = db.get_fabricators()
+        return render_template("index.html", fabricators = fabricators)
 
-
-@app.route('/users', methods=["GET", "POST"])
-
-def get_users():
+#Ruta de administrador
+@app.route('/admin', methods=["GET", "POST"])
+def admin():
     if request.method == "GET":
-        #Si no viene nada en el id quiere decir que solo quiere obtener usuarios, sino quiere eliminar
         if request.args.get('id') == None:
-            users = db.get_users()
-            return render_template('users.html', users = users)
-        elif request.args.get('id') != None:
-            #Si el metodo es delete, se elimina por id, sino puedo hacer alguna otra cosa
-            if request.args.get('method') == "delete":
-                id = request.args.get('id')
-                print(request.args.get('method'))
-                print("pasamos method")
-                db.delete_user(id)
-                return redirect('/users')
-            
+            return render_template('admin.html')        
 
-@app.route('/users/add', methods=["GET", "POST"])
-def show_forms():
+                                        #Ruta para panel de usuarios
+@app.route('/admin/users', methods=["GET", "POST"])
+
+def users():
     if request.method == "GET":
-        return render_template("addusers.html")
-    elif request.method == "POST":
+        lastUsers = db.last_users()
+        return render_template('users.html', lastUsers = lastUsers)
+
+
+#Ruta para obtener todos los usuarios/actualizar/eliminar
+@app.route('/admin/users/all', methods=["GET", "POST"])
+def get_users():
+        if request.method == "GET":
+        #Si no viene nada en el id quiere decir que solo quiere obtener usuarios, sino quiere eliminar
+            if request.args.get('id') == None:
+                users = db.get_users()
+                return render_template('all_users.html', users = users)
+            elif request.args.get('id') != None:
+                #Si el metodo es delete, se elimina por id, sino puedo hacer alguna otra cosa
+                id = request.args.get('id')
+                if request.args.get('method') == "delete":
+                    db.delete_user(id)
+                    return redirect('/admin/users/all')
+                if request.args.get('method') == 'update':
+                    user = db.get_user(id)
+                    return render_template("update_user.html", user = user)
+#Ruta para actualizar usuario
+@app.route('/admin/users/update', methods = ["GET", "POST"])
+def update_user():
+    if request.method == "POST":
+        req = request.form
+        db.update_user(req["id"], req["name"], req["surname"], req["email"], req["phone"])
+        return render_template("success.html", updatedUser = req["name"] )
+                    
+    
+#Ruta para agregar un usuario
+@app.route('/admin/users/add', methods=["GET", "POST"])
+def show_forms():
+    if request.method == "POST":
         print("Estan agregando un usuario")
         print(request.form)
         print(request.form["name"])
         req = request.form
         db.add_user(req["name"], req["surname"], req["email"],req["phone"])
         return render_template('success.html', user = req["name"])
+    else:
+        return render_template('404.html')
+                                            #Ruta para panel de productos
 
-@app.route('/products', methods=["GET", "POST"])
+@app.route('/admin/products', methods=["GET", "POST"])
 def products():
+    fabricators = db.get_fabricators()
+    lastProducts = db.last_products()
+    return render_template('products.html', fabricators = fabricators, lastProducts = lastProducts)
+#Ruta para ver todos los productos
+@app.route('/admin/products/all', methods = ["GET", "POST"])
+def get_products():
+    if request.args.get('id') == None:
+        if request.method == "GET":
+            products = db.get_products()
+            return render_template('all_products.html', products = products)
+    elif request.args.get('id') != None:
+        id = request.args.get('id')
+        if request.args.get('method') == 'delete':
+            db.delete_product(id)
+            return redirect('/admin/products/all')
+        elif request.args.get('method') == 'update':
+            product = db.get_product(id)
+            fabricators = db.get_fabricators()
+            return render_template('update_product.html', product = product, fabricators = fabricators)
+
+#Ruta para agregar productos
+@app.route('/admin/products/add', methods=["GET", "POST"])
+def add_products():
     if request.method == "GET":
-        products = db.get_products()
-        return render_template('products.html', products = products)
+        return render_template("404.html")
+    elif request.method == "POST":
+        print("Estan agregando un repuesto")
+        print(request.form)
+        req = request.form
+        db.add_spare(req["name"], req["fabricator"], req["observation"])
+        return render_template('success.html', spare = req["name"]) 
+#Ruta para actualizar repuestos
+@app.route('/admin/products/update', methods=["POST"])
+def update_products():
+    if request.method == "POST":
+        req = request.form
+        print(req)
+        db.update_product(req['id'], req['name'], req['fabricator'], req['observation'])
+        return render_template('success.html',updatedSpare = req['name'] )
+
+#Ruta para acceder a clientes
+@app.route('/admin/fabricators/all', methods=["GET", "POST"])
+
+def get_fabricators():
+    if request.method == "GET":
+        if request.args.get("id") == None:
+            fabricators = db.get_fabricators()
+            return render_template("all_fabricators.html", fabricators = fabricators)
+        else:
+            id = request.args.get("id")
+            method = request.args.get("method")
+            if method == "delete":
+                db.delete_fabricator(id)
+                return redirect('/admin/fabricators/all')
+            else:
+                fabricator = db.get_fabricator(id)
+                return render_template('update_fabricator.html', fabricator = fabricator)
+@app.route('/admin/fabricators/update', methods=["GET", "POST"])
+def update_fabricator():
+    if request.method == "POST":
+        req = request.form
+        db.update_fabricator(req["id"], req["name"], req["country"])
+        return render_template("success.html", updatedFabricator = req["name"])
+
+
+@app.route('/admin/fabricators', methods=["GET", "POST"])
+def fabricators():
+    if request.method == "GET":
+        lastFabricators = db.last_fabricators()
+        return render_template("fabricators.html", lastFabricators = lastFabricators)
+
+@app.route('/admin/fabricators/add', methods=["GET", "POST"])
+def add_fabricator():
+    if request.method == "POST":
+        req = request.form
+        db.add_fabricator(req["name"], req["country"])
+        fabricator = req["name"]
+        return render_template("success.html", fabricator = fabricator)
+@app.route('/quotation/add', methods=["GET", "POST"])
+def add_quotation():
+    if request.method == "POST":
+        req = request.form
+        print(req)
+        user = db.get_user_by_email(req["email"])
+        if user == None:
+            db.add_user(req["name"], req["surname"], req["email"], req["phone"])
+            user = db.get_user_by_email(req["email"])
+            db.add_quotation(user[0], req["message"], req["fabricante"], req["model"])
+            return("La cotizacion {} ha sido añadida al usuario NUEVO {} id {}".format(req["message"], user[1], user[0]))   
+        else:
+            db.add_quotation(user[0], req["message"], req["fabricante"], req["model"])
+            return("La cotizacion {} ha sido añadida al usuario EXISTENTE {} id {}".format(req["message"], user[1], user[0]))
+@app.route('/admin/quotations', methods = ["GET", "POST"])
+def quotations():
+    if request.method == "GET":
+        lastQuotations = db.last_quotations()
+        return render_template('quotations.html', lastQuotations = lastQuotations)
+@app.route('/admin/quotations/all', methods=["GET", "POST"])
+def all_quotations():
+    if request.method == "GET":
+        if request.args.get("id") == None:
+                quotations = db.get_quotations()
+                return render_template("all_quotations.html", quotations = quotations)      
+        else:
+            id = request.args.get("id")
+            method = request.args.get("method")
+            if method == "view":
+                quote = db.get_message(id)
+                return render_template("quote.html", quote = quote)
+              
+
+
+
+
+
+
 
 
 
--- database/database.py
+++ database/database.py
@@ -1,3 +1,4 @@
+from os import error
 import pymysql
 
 
@@ -23,9 +24,47 @@ class DataBase:
             
         except ValueError:
             print("Ha ocurrido un error")
+    #Seleccionar un cliente
+    def get_user(self, id):
+        try:
+            if id == None:
+                raise Exception("No ha ingresado ningun id")
+            else:
+                sql = "SELECT * FROM cliente WHERE id={}".format(id)
+                self.cursor.execute(sql)
+                self.connection.commit()
+                user = self.cursor.fetchone()
+                return user
+        except Exception:
+            raise
+    #Seleccionar un cliente por email
+    def get_user_by_email(self, email):
+        try:
+            if email == None:
+                raise Exception("No se ha ingresado ningún email")
+            else:
+                sql = "SELECT * FROM cliente WHERE email = '{}'".format(email)
+                self.cursor.execute(sql)
+                self.connection.commit()
+                user = self.cursor.fetchone()
+                return user
+        except Exception:
+            raise
+    #actualizar un cliente
+    def update_user(self, id, nombre, apellido, email, telefono):
+        try:
+            if id == None:
+                raise Exception
+            else:
+                sql = "UPDATE cliente SET nombre = '{}', apellido = '{}', email = '{}', telefono = '{}' WHERE id='{}'".format(nombre, apellido, email, telefono, id)
+                self.cursor.execute(sql)
+                self.connection.commit()
+
+        except Exception:
+            raise
     #Seleccionar TODOS clientes        
     def get_users(self):
-        sql ="SELECT * FROM cliente"
+        sql ="SELECT * FROM cliente ORDER BY id ASC"
 
         try:
 
@@ -127,20 +166,176 @@ class DataBase:
                 self.connection.commit()
         except Exception:
             raise
-    #Productos
-    def get_products(self):
+    #Last users
+    def last_users(self):
         try:
-            sql = "SELECT * FROM repuesto"
+            sql = "SELECT * FROM cliente ORDER BY id DESC LIMIT 5;"
             self.cursor.execute(sql)
-            products = self.cursor.fetchall()
-            return products
-
-            
-
-
+            self.connection.commit()
+            lastUsers = self.cursor.fetchall()
+            return lastUsers
         except Exception:
             raise
 
+    #Fabricantes
+    def get_fabricators(self):
+        try:
+            sql = "SELECT * FROM fabricante"
+            self.cursor.execute(sql)
+            self.connection.commit()
+            fabricators = self.cursor.fetchall()
+            return fabricators
+        except Exception:
+            raise
+    #Repuesto
+    def add_spare(self, name, fabricator, observation):
+        sql ="INSERT INTO repuesto (nombre, fabricante_id, observacion) VALUES ('{}', '{}', '{}')".format(name, fabricator, observation)
+
+        try:
+            self.cursor.execute(sql)
+            self.connection.commit()
+            
+        except ValueError:
+            print("Ha ocurrido un error")
+    def get_products(self):
+        try:
+            sql = "SELECT repuesto.id AS ID, repuesto.nombre AS Nombre, fabricante.nombre AS Fabricante, repuesto.observacion AS Observacion FROM repuesto INNER JOIN fabricante ON fabricante.id = repuesto.fabricante_id ORDER BY id ASC"
+            self.cursor.execute(sql)
+            products = self.cursor.fetchall()
+            return products
+        except Exception:
+            raise
+    def last_products(self):
+        try:
+            sql = "SELECT repuesto.id AS ID, repuesto.nombre AS Nombre, fabricante.nombre AS Fabricante, repuesto.observacion AS Observacion FROM repuesto INNER JOIN fabricante ON fabricante.id = repuesto.fabricante_id ORDER BY id DESC LIMIT 5;"
+            self.cursor.execute(sql)
+            self.connection.commit()
+            lastProducts = self.cursor.fetchall()
+            return lastProducts
+        except Exception:
+            raise
+    #obtener un producto
+    def get_product(self, id):
+        try:
+            if id == None:
+                raise Exception("No ha ingresado ningún id")
+            else:
+                sql = "SELECT * FROM repuesto WHERE id={}".format(id)
+                self.cursor.execute(sql)
+                self.connection.commit()
+                product = self.cursor.fetchone()
+                return product
+        except Exception:
+            raise
+    #actualizar un producto
+    def update_product(self, id, nombre, fabricante_id, observacion):
+        try:
+            if id == None:
+                raise Exception("No se ha ingresado ningún id")
+            else:
+                sql = "UPDATE repuesto SET nombre = '{}', fabricante_id = '{}', observacion = '{}' WHERE id= {}".format(nombre, fabricante_id, observacion, id)
+                self.cursor.execute(sql)
+                self.connection.commit()
+        except Exception:
+            raise
+    #Eliminar producto
+    def delete_product(self, id):
+        try:
+            if id == None:
+                raise Exception("No se ha ingresado ningún id")
+            else:
+                sql = "DELETE FROM repuesto WHERE id = {}".format(id)
+                self.cursor.execute(sql)
+                self.connection.commit()
+        except Exception:
+            raise
+    #Eliminar un fabricante
+    def delete_fabricator(self, id):
+        try:
+            if id == None:
+                raise Exception("No se ha ingresado ningún id")
+            else:
+                sql = "DELETE FROM fabricante WHERE id= {}".format(id)
+                self.cursor.execute(sql)
+                self.connection.commit()
+        except Exception:
+            raise
+    def update_fabricator(self, id, name, country):
+        try:
+            if id == None:
+                raise Exception("No ha ingresado ningún id")
+            else:
+                sql = "UPDATE fabricante SET nombre = '{}', pais = '{}' WHERE id ={}".format(name, country, id)
+                self.cursor.execute(sql)
+                self.connection.commit()
+        except Exception:
+            raise 
+    def get_fabricator(self, id):
+        try:
+            if id == None:
+                raise Exception("No ha ingresado ningún id")
+            else:
+                sql = "SELECT * FROM fabricante WHERE id={}".format(id)
+                self.cursor.execute(sql)
+                self.connection.commit()
+                fabricator = self.cursor.fetchone()
+                return fabricator
+        except Exception:
+            raise
+    def last_fabricators(self):
+        try:
+            sql = "SELECT * FROM fabricante ORDER BY id DESC LIMIT 5;"
+            self.cursor.execute(sql)
+            self.connection.commit()
+            lastFabricators = self.cursor.fetchall()
+            return lastFabricators
+        except Exception:
+            raise
+    def add_fabricator(self, name, country):
+        try:
+            if name == None or country == None:
+                raise Exception("No se han ingresado suficientes datos")
+            else:
+                sql = "INSERT INTO fabricante (nombre, pais) VALUES ('{}','{}')".format(name, country)
+                self.cursor.execute(sql)
+                self.connection.commit()
+        except Exception:
+            raise
+    def add_quotation(self, cliente_id, mensaje, fabricante, modelo):
+        try:
+            if cliente_id == None:
+                raise Exception("No hay id al cual agregar cotizacion")
+            else:
+                sql = "INSERT INTO cotizacion (cliente_id, mensaje, fabricante, modelo) VALUES ({}, '{}', '{}', '{}')".format(cliente_id, mensaje, fabricante, modelo)
+                self.cursor.execute(sql)
+                self.connection.commit()
+        except Exception:
+            raise
+    #Last quotations
+    def last_quotations(self):
+        try:
+            sql = "SELECT cotizacion.id as id, cliente.nombre as Cliente,cotizacion.fabricante as Fabricante, cotizacion.modelo as Modelo, cotizacion.mensaje as Mensaje FROM cotizacion INNER JOIN cliente ON cliente.id = cotizacion.cliente_id ORDER BY id DESC LIMIT 6;"
+            self.cursor.execute(sql)
+            self.connection.commit()
+            lastQuotations = self.cursor.fetchall()
+            return lastQuotations
+        except Exception:
+            raise
+    #todas las cotizaciones
+    def get_quotations(self):
+        sql = "SELECT cotizacion.id as id, cliente.id as id_cliente, cliente.nombre as Cliente, cotizacion.fabricante as Fabricante, cotizacion.modelo as Modelo FROM cotizacion INNER JOIN cliente ON cliente.id = cotizacion.cliente_id"
+        self.cursor.execute(sql)
+        self.connection.commit()
+        quotations = self.cursor.fetchall()
+        return quotations
+    #obtener texto cotizacion
+    def get_quote(self, id):
+        sql = "SELECT * FROM cotizacion WHERE id = {}".format()
+        self.cursor.execute(sql)
+        self.connection.commit()
+        quote = self.cursor.fetchone()
+        return quote
+        
 
 
     
